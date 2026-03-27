a687560da0007df973ed2b815532ac1ccdb5483b
Kv1st3n <233747346+Kv1st3n@users.noreply.github.com>
2026-03-27 22:15:45 +0100

    Changed so it uses flags now

--- main.go
+++ main.go
@@ -1,19 +1,18 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
+	"strings"
 )
 
 func main() {
 
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: bush-viper <mode> <ip> <port/prefix>")
-		fmt.Println("Modes: 1 (DNS), 2 (Single), 3 (Wide/Rust), 4 (Manual)")
-		return
-	}
+	mode := flag.Int("mode", 1, "Modes: 1 (DNS), 2 (Single), 3 (Wide/Rust)")
+	ip := flag.String("ip", "127.0.0.1", "Target IP Adress")
+	port := flag.String("port", "80", "Port (e.g., '80' for mode 2, or '80-90' for mode 3)")
 
-	modeChoice := os.Args[1]
+	flag.Parse()
 
 	const (
 		ColorGreen = "\033[32m"
@@ -37,60 +36,40 @@ func main() {
 	fmt.Println(ColorGreen + underLine + ColorReset)
 	fmt.Println("V1.0.0 - Starting Viper Engine...")
 
-	switch modeChoice {
-	case "1":
+	switch *mode {
+	case 1:
 
-		if len(os.Args) < 3 {
-			fmt.Println("Error: Mode 1 requires a hostname. Example: ./bush-viper 1 google.com")
-			return
-		}
-		host := os.Args[2]
-		ipAddress, _ := getHostAddress(host)
-		fmt.Printf("DNS result for %s: %v\n", host, ipAddress)
+		ipAddress, _ := getHostAddress(*ip)
+		fmt.Printf("DNS result for %s: %v\n", *ip, ipAddress)
 
-	case "2":
+	case 2:
 
-		if len(os.Args) < 4 {
-			fmt.Println("Error: Mode 2 requires IP and Port. Example: ./bush-viper 2 127.0.0.1 80")
-		}
+		result, err := singlePortScan(*ip, *port)
 
-		ip := os.Args[2]
-		port := os.Args[3]
-
-		result, err := singlePortScan(ip, port)
 		if err != nil || result.Opened == "" {
-			fmt.Printf("[-] %s:%s is CLOSED or Unreachable\n", ip, port)
+			fmt.Printf("[-] %s:%s is CLOSED or Unreachable\n", *ip, *port)
 		} else {
 			fmt.Printf("[+] %s:%s is OPEN!\n", result.IP, result.Port)
 
 			if result.Banner != "" {
 				fmt.Printf("    Banner: %q\n", result.Banner)
-			} else {
-				fmt.Println("    Banner: <No response or no probe matches>")
 			}
 		}
 
-	case "3":
-		var startPort string = "1"
-		var endPort string = "65536"
+	case 3:
+		startPort, endPort := "1", "65536"
 
-		if len(os.Args) < 3 {
-			fmt.Println("Error: Mode 3 requires IP. Example: ./bush-viper 3 127.0.0.1")
-			return
-		}
-		ip := os.Args[2]
-
-		if len(os.Args) >= 4 {
-			startPort = os.Args[3]
+		if strings.Contains(*port, "-") {
+			parts := strings.Split(*port, "-")
+			startPort = parts[0]
+			endPort = parts[1]
+		} else {
+			startPort = *port
 		}
 
-		if len(os.Args) >= 5 {
-			endPort = os.Args[4]
-		}
+		fmt.Printf("[*] Starting wide port scan on %s (Ports %s-%s)...\n", *ip, startPort, endPort)
 
-		fmt.Printf("[*] Starting wide port scan on %s (Ports %s-%s)...\n", ip, startPort, endPort)
-
-		results, err := widePortScan(ip, startPort, endPort)
+		results, err := widePortScan(*ip, startPort, endPort)
 		if err != nil {
 			fmt.Printf("[-] Error during wide scan: %v\n", err)
 			return
@@ -110,8 +89,10 @@ func main() {
 			}
 		}
 
-	case "4":
+	case 4:
 		fmt.Println("Manual")
+	default:
+		fmt.Printf("Unknown mode: %d. Use -help for usage.\n", *mode)
 	}
 
 }
